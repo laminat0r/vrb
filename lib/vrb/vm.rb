28e81f5d91963a690f4b8a9ad8f53e52e2ac221e
Mick Pollard <aussielunix@gmail.com>
2013-09-11 16:55:40 +1000

    add private method for listing a vms nics

--- lib/vrb/vm.rb
+++ lib/vrb/vm.rb
@@ -29,7 +29,31 @@ module Vrb
     private
 
     def list_nics
-      nil
+      nics = []
+      nic_mobs = @mob.config.hardware.device.select do |n|
+        n.deviceInfo.label =~ /Network\ adapter/
+      end
+      nic_mobs.each do |n|
+        # if the nic is on a dvSwitch, we have to go to it to find the name of the port group
+        if n.backing.class.to_s =~ /VirtualEthernetCardDistributedVirtualPortBackingInfo/
+          pgs = @mob.runtime.host.network.grep(VIM::DistributedVirtualPortgroup)
+          network = pgs.find { |p| p.key == n.backing.port.portgroupKey }
+          dvswitch = pgs.collect { |p| p.config.distributedVirtualSwitch }
+        end
+
+        nic_name = n.deviceInfo.label
+        nic_friendly_name = nic_name.sub(/Network\ adapter\s+/, 'nic')
+
+        nics[nic_friendly_name] = {
+          :name => nic_name,
+          :network_name => network.name,
+          :network_mob => network,
+          :nic_mob => n,
+          :dvswitch => dvswitch,
+          :is_connected => n.connectable.connected
+        }
+        nics
+      end
     end
   end
 end
