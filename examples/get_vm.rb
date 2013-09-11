c990f7721cfce137251d19fde33870bf8888552f
Mick Pollard <aussielunix@gmail.com>
2013-09-11 13:58:21 +1000

    updated get_vm example

--- examples/get_vm.rb
+++ examples/get_vm.rb
@@ -3,21 +3,17 @@
 
 require 'vrb'
 
-datacenter  = 'B0'
-vm_name     = 'Bulletproof/Templates/Bx - Ubuntu 12.04 LTS x64 - VMv8 - v1.0'
-
 vc = Vrb::Vcenter.new
-dc = vc.get_datacenter(datacenter)
+dc = vc.get_datacenter('B0') # can be long or short form - something that is unique
+vm = dc.get_vm('Bulletproof/Templates/Bx - Ubuntu 12.04 LTS x64 - VMv8 - v1.0')
 
-vm = dc.get_vm(vm_name)
 
-# the Vm object has a sensible set of default data and methods available but you have access to the underlying 
-# rbvmomi object should you want it - vm.mob.***
-
-puts "--- Name:     #{vm_name} ---"
-puts "--- Template: #{vm.is_a_template} ---"
-puts "--- OS:         #{vm.mob.summary.config.guestFullName} ---"
-puts "--- CPU:        #{vm.mob.summary.config.numCpu} ---"
-puts "--- RAM:        #{vm.mob.summary.config.memorySizeMB} ---"
+puts "--- SERVICEID:  #{vm.serviceid} ---"
+puts "--- CPU:        #{vm.cpu} ---"
+puts "--- RAM:        #{vm.ram} ---"
 puts "--- Annotation: #{vm.mob.summary.config.annotation} ---"
+puts "--- Template:   #{vm.is_a_template} ---"
+puts "--- Host:       #{vm.host} ---"
+puts "--- IP:         #{vm.ip} ---"
+puts "--- OS:         #{vm.os} ---"
 
