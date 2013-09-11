2b0c7f45c3160dc8b840708fc65d8d5f02e2351e
Mick Pollard <aussielunix@gmail.com>
2013-09-11 13:29:19 +1000

    add :serviceid, :ram, :cpu to Vrb::VM class

--- lib/vrb/vm.rb
+++ lib/vrb/vm.rb
@@ -3,23 +3,26 @@
 module Vrb
   class VM < VrbObject
 
-    attr_reader :mob, :tools_status, :os, :ip, :host, :is_a_template, :nics
+    attr_reader :mob, :tools_status, :os, :ip, :host, :is_a_template, :nics, :serviceid, :ram, :cpu
 
     def initialize(parent_mob, self_mob)
       @mob = self_mob
       @parent_mob = parent_mob
       @nics = list_nics
       begin
-        @tools_status = @mob.guest.toolsStatus
-        @os = @mob.summary.guest.guestFullName
-        @ip = @mob.guest.ipAddress
-        @host = @mob.runtime.host.name
-        @is_a_template = @mob.summary.config.template
+        @serviceid      = @mob.name
+        @tools_status   = @mob.guest.toolsStatus
+        @cpu            = @mob.config.hardware.numCPU
+        @ram            = @mob.config.hardware.memoryMB
+        @os             = @mob.summary.guest.guestFullName
+        @ip             = @mob.guest.ipAddress
+        @host           = @mob.runtime.host.name
+        @is_a_template  = @mob.summary.config.template
       rescue
-        @os = nil
-        @ip = nil
-        @host = nil
-        @is_a_template = nil
+        @os             = nil
+        @ip             = nil
+        @host           = nil
+        @is_a_template  = nil
       end
     end
   end
