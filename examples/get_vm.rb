cb102d18f93cc86da4865b7c348fb8ec13354d60
Mick Pollard <aussielunix@gmail.com>
2013-09-12 13:43:05 +1000

    add bunch of examples

--- examples/get_vm.rb
+++ examples/get_vm.rb
@@ -4,8 +4,8 @@
 require 'vrb'
 
 vc = Vrb::Vcenter.new
-dc = vc.get_datacenter('B0') # can be long or short form - something that is unique
-vm = dc.get_vm('Bulletproof/Templates/Bx - Ubuntu 12.04 LTS x64 - VMv8 - v1.0')
+dc = vc.datacenter('B0') # can be long or short form - something that is unique
+vm = dc.vm('Bulletproof/Templates/Bx - Ubuntu 12.04 LTS x64 - VMv8 - v1.0')
 
 
 puts "--- SERVICEID:  #{vm.serviceid} ---"
