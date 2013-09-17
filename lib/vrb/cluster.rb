a5328f8c8a44ee237fdc45ed609831adc35a5b25
Mick Pollard <aussielunix@gmail.com>
2013-09-17 20:58:06 +1000

    add methods for listing resource_pools and finding a specific resource_pool

--- lib/vrb/cluster.rb
+++ lib/vrb/cluster.rb
@@ -9,5 +9,21 @@ module Vrb
       @mob = self_mob
       @parent_mob = parent_mob
     end
+
+    def resource_pools(return_as_mobs = true)
+      mobs = @mob.resourcePool.resourcePool
+      if return_as_mobs == true
+        mobs
+      else
+        mobs.collect { |mob| mob.name }
+      end
+    end
+
+    def resource_pool(name)
+      mobs = self.resource_pools
+      rp_mob = mobs.find { |mob| mob.name =~ /#{name}/ }.first or fail "Sorry!"
+      ResourcePool.new(@mob,rp_mob)
+    end
+
   end
 end
