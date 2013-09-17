0518b77a69abb7494b13abe8199b35c09ae63053
Mick Pollard <aussielunix@gmail.com>
2013-09-17 20:57:27 +1000

    add methods for listing clusters and finding a specific cluster

--- lib/vrb/datacenter.rb
+++ lib/vrb/datacenter.rb
@@ -14,5 +14,20 @@ module Vrb
       vm_mob = @mob.find_vm(name) or fail "vm not found"
       VM.new(@mob, vm_mob)
     end
+
+    def cluster(name)
+      mobs = self.clusters
+      cl_mob = mobs.find { |mob| mob.name =~ /#{name}/ }.first or fail "Sorry!"
+      Cluster.new(@mob, cl_mob)
+    end
+
+    def clusters(return_as_mobs = true)
+      mobs = @mob.hostFolder.children.grep(VIM::ClusterComputeResource)
+      if return_as_mobs == true
+        mobs
+      else
+        mobs.collect { |mob| mob.name }
+      end
+    end
   end
 end
